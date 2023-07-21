a448e43f0f76a2e07e3387c142e339efcf41de0d
Ihor Safronov Note <igor130298@gmail.com>
2023-07-21 17:03:50 +0300

    continue work TelegramBot

---
+++ TelegramBot/test.py
@@ -0,0 +1,8 @@
+import random
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message, KeyboardButton
+from aiogram.filters import Text, Command
+import os
+import dotenv
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
--- test.py
+++
@@ -1,3 +0,0 @@
-from random import randint, choice, randrange
-
-print(randrange(1,100))
