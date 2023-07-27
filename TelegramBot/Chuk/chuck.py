d173f9382ac79821f6e7f198b7eb2b519956a87f
Ihor Safronov Note <igor130298@gmail.com>
2023-07-27 23:47:35 +0300

    chuck_bot work v2

--- TelegramBot/Chuk/chuck.py
+++ TelegramBot/Chuk/chuck.py
@@ -1,30 +1,46 @@
-import requests
-import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.filters import Text, Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.filters import CommandStart, Text
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+import requests
 
-BOT_TOKEN: str = '6303185352:AAECZ7vk1ZS60d4d02HpT3G6arOvqPa3iUU'
+chuck_joke_categories = requests.get('https://api.chucknorris.io/jokes/categories').json()
 
-bot: Bot = Bot(BOT_TOKEN)
+
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+API_TOKEN: str = '6303185352:AAECZ7vk1ZS60d4d02HpT3G6arOvqPa3iUU'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
+kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+buttons: list[InlineKeyboardButton] = [InlineKeyboardButton(text=i,callback_data=i) for i in chuck_joke_categories]
+buttons[0] = InlineKeyboardButton(text='random', callback_data='random')
+kb_builder.row(*buttons, width=2)
+keyboard = kb_builder.as_markup()
 
-
-@dp.message(Command(commands=['start']))
+# Этот хэндлер будет срабатывать на команду "/start"
+# и отправлять в чат клавиатуру с инлайн-кнопками
+@dp.message(CommandStart())
 async def process_start_command(message: Message):
-    url_random_chuck: str = 'https://api.chucknorris.io/jokes/random'
-    random_chuck: str = requests.get(url_random_chuck).json()['value']
-    await message.answer(random_chuck)
+    await message.answer(text='Это инлайн-кнопки. Нажми на любую!',
+                         reply_markup=keyboard)
 
-@dp.message(Command(commands=['help']))
-async def process_start_help(message: Message):
-    await message.answer('random chuck joke')
+@dp.callback_query(Text(text=['random']))
+async def random_chuck_joke(callback: CallbackQuery):
+    chuck_random_joke = requests.get('https://api.chucknorris.io/jokes/random').json()['value']
+    await callback.message.answer(f'Random Chuck joke\n{chuck_random_joke}')
+    await callback.answer()
 
-@dp.message()
-async def process_other_text_answers(message: Message):
-    await message.answer('I don\'t know what you\'re talking about')
 
+@dp.callback_query()
+async def press_category(callback: CallbackQuery):
+    chuck_joke = requests.get(f'https://api.chucknorris.io/jokes/random?category={callback.data}').json()['value']
+    await callback.message.answer(f'joke-category: {callback.data}\n{chuck_joke}')
+    await callback.answer()
 
 if __name__ == '__main__':
     dp.run_polling(bot)
